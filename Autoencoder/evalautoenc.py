8dc3c4e4e2c17a8c7346c484765d21882ff526e4
Seoleun Shin <125943805+seoleuns@users.noreply.github.com>
2025-05-28 10:05:55 +0900

    Update evalautoenc.py

--- Autoencoder/evalautoenc.py
+++ Autoencoder/evalautoenc.py
@@ -173,8 +173,8 @@ rmse=[]
 fsim=[]
 uiq=[]
 i = 0
-file_list1  = renormal_load_image_from_dir2('./Data/Noisy')
-file_list2  = renormal_load_image_from_dir2('./Data/Clean')
+file_list1  = renormal_load_image_from_dir2('../Data/Line/Noisy')
+file_list2  = renormal_load_image_from_dir2('../Data/Line/Clean')
 
 for file1, file2 in zip(file_list1, file_list2):
     file1  = (file1.reshape(1, target_height,target_width,1))
